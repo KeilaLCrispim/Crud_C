541087012e62c9ce6fec8ea8bdcb5dc73de77a1e
Danilo Olivera <sdanilo109@gmail.com>
2020-03-26 16:07:30 -0300

    Utilizando Modais no Crud

--- C#_CRUD/Controllers/UsuariosCOntrollers.cs
+++ C#_CRUD/Controllers/UsuariosCOntrollers.cs
@@ -10,13 +10,13 @@ using Microsoft.Data.SqlClient;
 using C__CRUD.Services;
 
 namespace C__CRUD.Controllers
-{
+{   
+    [Route("Usuario")]
     public class UsuariosController : Controller
     {
         Conexao conexao = new Conexao();
         SqlCommand cmd = new SqlCommand();
-        private readonly Contexto _contexto;
-
+        private Contexto _contexto;
         public UsuariosController(Contexto contexto)
         {
             _contexto = contexto;
@@ -25,92 +25,63 @@ namespace C__CRUD.Controllers
         // Aqui criamos um lista onde ela ja tem pessoas cadastradas, mas não é possivel excluir e alterar
         List<UsuarioServices> usuarios = UsuarioServices.GetUsuarioServices();
 
+        [Route("")]
+        [Route("~/")]
+        [Route("index")]
         public IActionResult Index()
         {
-            // Pessoas ja cadastradas no SQL Server
-            //cmd.Connection = conexao.Conectar();
-            //cmd.CommandText = "insert into Usuario (Nome, Email, Senha) values('Danilo', 'Ahrkan@Ahrkan', '1234')";
-            //cmd.ExecuteNonQuery(); // Esse comando manda a informação para o SQL Serve
-            //conexao.Desconctar();
-
-            var lista = _contexto.Usuario.ToList();
-
-            // Nesse "return" e possivel escolher dois retornos, 1° a variavel "lista" ela tras as pessoas cadastradas no DB.
-            // 2° a variavel "usuarios" nela retornamos as Pessoas ja cadastradas, mas essas pessoas são imutaveis.
-            return View(usuarios);
-        }
-
-        [HttpGet]
-        public IActionResult Create()
-        {
-            var usuario = new Usuario();
-            return View(usuario);
+            ViewBag.usuario = _contexto.Usuario.ToList();
+            return View();
         }
 
         [HttpPost]
-        public IActionResult Create(Usuario usuario)
+        [Route("Create")]
+        public IActionResult Create(string nomeAcionar, string emailAcionar, string senhaAcionar)
         {
             if (ModelState.IsValid)
             {
+                var usuario = new Usuario{
+                    NomeUsuario = nomeAcionar,
+                    Email = emailAcionar,
+                    Senha = senhaAcionar
+                };
                 _contexto.Usuario.Add(usuario);
                 _contexto.SaveChanges();
 
                 return RedirectToAction("Index");
             }
-            return View(usuario);
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
-        public IActionResult Edit(int Id)
+        [Route("find/{id}")]
+        public IActionResult Edit(int id)
         {
-            var usuario = _contexto.Usuario.Find(Id);
-            return View(usuario);
+            var usuarioEdit = _contexto.Usuario.Find(id);
+            return new JsonResult(usuarioEdit);
         }
 
         [HttpPost]
-        public IActionResult Edit(Usuario usuario)
+        [Route("Edit")]
+        public IActionResult Edit(int id, string nomeUsuario, string email, string senha)
         {
-            if (ModelState.IsValid)
-            {
-                _contexto.Usuario.Update(usuario);
-                _contexto.SaveChanges();
-
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View(usuario);
-            }
-        }
-
-        [HttpGet]
-        public IActionResult Delete(int Id)
-        {
-            var usuario = _contexto.Usuario.Find(Id);
-            return View(usuario);
+            var usuarioEdit = _contexto.Usuario.Find(id);
+            usuarioEdit.NomeUsuario = nomeUsuario;
+            usuarioEdit.Email = email;
+            usuarioEdit.Senha = senha;
+            _contexto.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public IActionResult Delete(Usuario _usuario)
+        [Route("Delete")]
+        public IActionResult Delete(int idDelete)
         {
-            var usuario = _contexto.Usuario.Find(_usuario.Id);
-            if (usuario != null)
-            {
-                _contexto.Usuario.Remove(usuario);
-                _contexto.SaveChanges();
-
-                return RedirectToAction("Index");
-            }
-            return View(usuario);
+            var usuarioDelete = _contexto.Usuario.Find(idDelete);
+            _contexto.Usuario.Remove(usuarioDelete);
+            _contexto.SaveChanges();
+            return RedirectToAction("Index");
         }
-
-        [HttpGet]
-        public IActionResult Details(int Id)
-        {
-            var usuario = _contexto.Usuario.Find(Id);
-            return View(usuario);
-        }
-
         /// <summary>
         /// 
         /// </summary>
